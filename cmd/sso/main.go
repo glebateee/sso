254df79554f84043e52b74b4b0c644fa343e10fa
glebateee <glebstanyul2003@gmail.com>
2025-11-20 15:32:40 +0300

    init config & logger

---
+++ cmd/sso/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
+
+	log := setupLogger(cfg.Env)
+	log.Debug("debug invoked")
+	log.Info("info invoked")
+
+}
+
+func setupLogger(currEnv string) *slog.Logger {
+	var log *slog.Logger
+	switch currEnv {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
+}
---
+++ internal/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string        `yaml:"env" env-default:"local"`
+	StoragePath string        `yaml:"storage_path" env-required:"true"`
+	TokenTTL    time.Duration `yaml:"token_ttl" env-default:"2h"`
+	GRPC        gRPC          `yaml:"grpc" env-required:"true"`
+}
+
+type gRPC struct {
+	Port    string        `yaml:"port" env-default:"12345"`
+	Timeout time.Duration `yaml:"timeout" env-default:"2s"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		panic("path to config file is required")
+	}
+	if _, err := os.Stat(configPath); err != nil {
+		panic("config file not found: " + configPath)
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		panic("error reading config file: " + err.Error())
+	}
+	return &cfg
+}
